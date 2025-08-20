6e295d5114b5995e649c5dd935e19f9a926d9cab
carlosdominguezfernandez <cadomi03@ucm.es>
2025-08-20 17:27:14 +0200

    main.py

---
+++ main.py
@@ -0,0 +1,36 @@
+#
+# Punto de entrada del algoritmo de satisfactibilidad
+#
+
+import sys
+
+import spot
+
+from parser import BaseParser
+from parityGame import ParityGame
+
+
+def main():
+	import argparse
+	parser = argparse.ArgumentParser()
+	parser.add_argument('< > False', help='Fórmula')
+
+	args = parser.parse_args()
+
+	# Parsea la fórmula
+	formula = BaseParser().parse(args.formula)
+
+	# Crea el APTA
+	pg = ParityGame.from_formula(formula)
+	pg_txt = pg._to_pgsolver_format()
+	# print(pg_txt)
+
+	pga, = spot.automata(pg_txt)
+
+	print(not spot.solve_game(pga))
+
+	return 0
+
+
+if __name__ == '__main__':
+	sys.exit(main()
